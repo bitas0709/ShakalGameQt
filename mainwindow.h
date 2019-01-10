90b62d1d6935de2db3cc0dc0e032e2f6ce12c652
bitas0709 <bitas0709@gmail.com>
2019-01-10 23:55:08 +0300

    Улучшил читаемость кода

--- mainwindow.h
+++ mainwindow.h
@@ -20,24 +20,32 @@ protected:
     void initializeGL();
     void resizeGL(int w, int h);
     void paintGL();
+
+    int playgroundWidth = 600, playgroundHeight = 600; //размеры игрового поля x и y
+    int windowWidth = playgroundWidth + 200, windowHeight = playgroundHeight; //размеры окна x и y
+    //int borderX = 10, borderY = 10; //размер рамок
+
     void drawBackground(); //отрисовка заднего фона игрового поля
     void drawp(); //отрисовка модели игрока
     void drawStat(); //отрисовка статистики игры
     void drawWinMessage(); //отрисовка окна с сообщением о выигрыше
     void drawActivatedCheat(); //отрисовка окна о вводе чит-кода
-    int x = 0, y = 0; //стартовые координаты
-    int playgroundWidth = 600, playgroundHeight = 600; //размеры игрового поля x и y
-    int windowWidth = playgroundWidth + 200, windowHeight = playgroundHeight; //размеры окна x и y
-    int borderX = 10, borderY = 10; //размер рамок
+
+    QFont msgText(); //шрифт сообщений
+
+    int x = 0, y = 0; //стартовые координаты модели игрока
+    qint8 score = 0; //счёт игрока
+
     virtual void keyPressEvent(QKeyEvent *event); //обработка событий клавиатуры
     virtual void keyReleaseEvent(QKeyEvent *event);
+    int key; //код нажатой клавиши
+
     bool win = false; //вывод текста о выигрыше на экран
     bool actCheat = false;
-    QFont msgText(); //шрифт сообщений
-    qint8 score = 0; //счёт игрока
+
     QString cheat; //строка для обработки ввода читов
+
     QTimer *timer = new QTimer(this);
-    int key; //код нажатой клавиши
     int time = 500; //скорость движения игрока
 
 signals:
